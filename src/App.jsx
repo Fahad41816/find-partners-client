7abfea72c4f78a0f568e86803180ca7723f7fbf6
fahad <99122881+fahad4161@users.noreply.github.com>
2023-11-21 16:39:15 +0600

    ready to create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+      <div className='text-red-500 font-bold text-xl animate-bounce'>
+        hello nahidul islam fahad good luck for file sure company task
+      </div>     
+    </>
+  )
+}
+
+export default App
